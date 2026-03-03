038562455380a4d47f0a092b2754bf6efbae8077
tabrejsiddi <139753435+tabrejsiddi@users.noreply.github.com>
2026-03-03 15:51:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
-//your code here!
+const list = document.getElementById("infi-list");
 
+let count = 0;
+
+function addItems(num) {
+  for (let i = 0; i < num; i++) {
+    count++;
+    const li = document.createElement("li");
+    li.textContent = `List Item ${count}`;
+    list.appendChild(li);
+  }
+}
+
+addItems(10);
+window.addEventListener("scroll", () => {
+  const scrollTop = window.scrollY;
+  const windowHeight = window.innerHeight;
+  const fullHeight = document.documentElement.scrollHeight;
+
+  if (scrollTop + windowHeight >= fullHeight - 5) {
+    addItems(2);
+  }
+});
